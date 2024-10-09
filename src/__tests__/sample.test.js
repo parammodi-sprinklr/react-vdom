c3fed1ff45c46c9b02ac9fd1c2be81eb501e8f8b
Param Modi <param.modi@sprinklr.com>
2024-10-09 16:01:11 +0530

    add sample tests

--- src/__tests__/sample.test.js
+++ src/__tests__/sample.test.js
@@ -1,10 +1,45 @@
-import App from "../App";
-import {render, screen} from "@testing-library/react";
-import "@testing-library/jest-dom";
+import { createElement } from '../react-functions/createElement';
+import { diff } from '../react-functions/diff';
+import { updateDOM } from '../react-functions/updateDOM';
+import { render } from '../react-functions/render';
 
+test('createElement should create a virtual DOM node', () => {
+  const vNode = createElement('div', { id: 'test' }, 'Hello World');
+  expect(vNode).toEqual({
+    type: 'div',
+    props: {
+      id: 'test',
+      children: ['Hello World'],
+    },
+  });
+});
 
-test("Should render Hello World", async () => {
-  render(<App />);
+test('diff should detect type change', () => {
+  const oldVNode = createElement('div', {});
+  const newVNode = createElement('span', {});
 
-  screen.getByText("Hello World");
+  const patches = diff(oldVNode, newVNode);
+  expect(patches).toEqual([{ type: 'REPLACE', newVNode }]);
+});
+
+test('updateDOM should replace an element', () => {
+  const parent = document.createElement('div');
+  const oldChild = document.createElement('div');
+  parent.appendChild(oldChild);
+
+  const patch = {
+    type: 'REPLACE',
+    newVNode: { type: 'span', props: {}, children: [] },
+  };
+  updateDOM(parent, [patch]);
+
+  expect(parent.firstChild.tagName).toBe('SPAN');
+});
+
+test('render should correctly render virtual DOM to real DOM', () => {
+  const vNode = createElement('div', {}, createElement('h1', {}, 'Hello'));
+  const container = document.createElement('div');
+  render(vNode, container);
+
+  expect(container.innerHTML).toBe('<div><h1>Hello</h1></div>');
 });
